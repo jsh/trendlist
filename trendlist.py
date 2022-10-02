a8c07fcf52e7ec244f46599a98d29661af88a9eb
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-10-02 09:35:50 -0600

    Check for error conditions, add type hinting.
    Add optinal type hinting.
    Check a few things and raise exceptions if they're violated.
    Tweak a few variable names for better readability.
    Pass isort, black, mypy, basic flake8.

--- trendlist.py
+++ trendlist.py
@@ -1,11 +1,17 @@
 """Define and manipulate trends."""
 # pylint: disable=fixme
 
-from collections import namedtuple
-from dataclasses import dataclass
+import collections
 import operator
 import random
-from typing import Iterable, Optional, Union
+from dataclasses import dataclass
+from typing import Iterable, List, Optional, Union
+
+
+# new static types
+Number = Union[float, int]
+Initializer = Union[Number, "Trend"]
+
 
 def pows(n: int, base: int = 2, start: int = 0) -> Iterable:
     """Generate sequences of powers of the base.
@@ -14,11 +20,12 @@ def pows(n: int, base: int = 2, start: int = 0) -> Iterable:
         e.g., start=3 will give the numbers in the order
         "3rd, 4th, ... nth, 0th, 1st, 2nd"
     """
-    start = start % n # in case start >= n
+    start = start % n  # in case start >= n
     for i in range(start, n):
         yield base**i
     for i in range(start):
         yield base**i
+
 
 def rands(n: int, seed: float = None, start: int = 0) -> Iterable:
     """Generate sequences of random floats.
@@ -32,14 +39,16 @@ def rands(n: int, seed: float = None, start: int = 0) -> Iterable:
     """
 
     random.seed(seed)
-    start = start % n         # in case start >= n
+    start = start % n  # in case start >= n
     for _ in range(start):
-        random.random()       # ignore first `start` numbers
+        random.random()  # ignore first `start` numbers
     for _ in range(start, n):
-        yield(random.random())
-    random.seed(seed)        # go back to the beginning
+        yield (random.random())
+    random.seed(seed)  # go back to the beginning
     for _ in range(start):
-        yield(random.random())
+        yield (random.random())
+    random.seed()  # reset the random-number generator
+
 
 @dataclass(order=True)
 class Trend:
@@ -53,66 +62,100 @@ class Trend:
         TypeError: Length is not an int
         ValueError: Length is not positive
 
-    TODO: write a post_init() to check for legit values in post_init
     """
-    mean: Optional[int] = None
+
+    mean: Number
     length: int = 1
 
-    def merge(self, other, reverse=False):
+    def __post_init__(self) -> None:
+        """Validate the object.
+
+        Raises:
+            TypeError: Length is not an int
+            ValueError: Length is not positive
+
+        """
+        if not isinstance(self.mean, (int, float)):
+            raise TypeError("mean must be number")
+        if not isinstance(self.length, int):
+            raise TypeError("length must be integer")
+        if self.length <= 0:
+            raise ValueError("length must be positive")
+
+    def merge(self, other, reverse=False) -> Optional["Trend"]:
+        if self.mean == other.mean:
+            raise ValueError("merging trend mean must differ!")
         can_merge = operator.gt if reverse else operator.lt
         if can_merge(self, other):
             length = self.length + other.length
-            # weighted mean
-            mean = (self.length*self.mean + other.length*other.mean)/length
+            sum = self.length * self.mean + other.length * other.mean
+            mean = sum / length
             return Trend(mean=mean, length=length)
         return None
 
-initializer = Union[float, int, "Trend"]
 
 class TrendList(list):
+    """A list of trends.
 
-    def __init__(self, s: list, reverse: bool = False):
-        self._reverse = reverse
+    Note that this sub-classes list.
+    """
+
+    def __init__(
+        self, s: Optional[List[Initializer]] = None, reverse: bool = False
+    ) -> None:  # noqa: E501
+        if s is None:
+            s = []
         for elem in s:
-            if not isinstance(elem, Trend): # accept either bTrends or numbers
+            if not isinstance(elem, Trend):  # accept either Trends or numbers
                 elem = Trend(elem)
             self.append(elem)
+        self._reverse = reverse
 
-    def append(self, other: "Trend"):
-        """Append a new trend.
+    def append(self, other: "Trend") -> None:
+        """Append a new trend, in-place.
 
         Add a Trend into a TrendList.
         Merge with the rightmost Trend object,
-        then continues recursively.
+        then continue recursively.
         """
+        if not isinstance(other, Trend):
+            raise TypeError("appended element must be Trend")
         if not self:
             super().append(other)
             return
         popped = self.pop()
-        if merged := popped.merge(other, reverse=self._reverse):  # merge and recurse
+        # if possible, merge and recurse
+        if merged := popped.merge(other, reverse=self._reverse):
             self.append(merged)
         else:  # new trend cannot merge
             self.extend([popped, other])  # push popped back on, then other
 
-    RotatedTrendList = namedtuple('RotatedTrendList', 'start rotations')
-
-    def rotate(self):
+    def rotate(self) -> "TrendList":
         """Move the leftmost trend to the right end, then merge."""
         merged = self
-        left = merged[0]
-        right = TrendList(merged[1:])
-        right.append(left)
-        merged = right
+        if len(merged) > 1:
+            left = merged[0]
+            right = TrendList(merged[1:])
+            right.append(left)
+            merged = right
         return merged
-        
-    def single(self):
+
+    """
+    After rotating a list, perhaps more than once,
+    how many rotations did you do,
+    and where, in the original list, was the current head?
+    """
+    Rotations = collections.namedtuple("Rotations", "start num_rots")
+
+    def single(self) -> Rotations:
         """Rotate until there's a single trend."""
         new = self
-        rotations = 0   # how many rotations to get to a single trend
-        orig_start = 0    # position in original list that will become new[0]
+        rotations = 0  # how many rotations to get to a single trend
+        orig_start = 0  # position in original list that will become new[0]
         while len(new) > 1:
-            orig_start += new[0].len  # positon in self that will become new[0] following rotation
+            orig_start += new[
+                0
+            ].len  # positon in self that will become new[0] following rotation
             new = new.rotate()
             rotations += 1
-        rtl = RotatedTrendList(start = orig_start, rotations=rotations)
-        return rtl
+        return self.Rotations(start=orig_start, num_rots=rotations)
