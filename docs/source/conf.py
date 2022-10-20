70e8f945ca49cc851af78f2b46aea3d540d3259e
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-10-20 17:35:52 -0600

    bump the patch version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Jeffrey S. Haemer'
 author = 'Jeffrey S. Haemer'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = '0.1.5'
 
 
 # -- General configuration ---------------------------------------------------
--- src/trendlist/__init__.py
+++ src/trendlist/__init__.py
@@ -1,4 +1,4 @@
 """Import all functions from trendlist module into package."""
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from .trendlist import *  # noqa
