e460e817008f34dd0a399761dadfb747cc88f9db
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-10-22 15:09:39 -0600

    Single quotes -> double quotes.
    Courtesy of black.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,12 +18,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'trendlist'
-copyright = '2022, Jeffrey S. Haemer'
-author = 'Jeffrey S. Haemer'
+project = "trendlist"
+copyright = "2022, Jeffrey S. Haemer"
+author = "Jeffrey S. Haemer"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.5'
+release = "0.1.5"
 
 
 # -- General configuration ---------------------------------------------------
@@ -39,12 +39,10 @@ extensions = [
     "sphinx.ext.intersphinx",
 ]
 
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None)
-}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -57,9 +55,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
