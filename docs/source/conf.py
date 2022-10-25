0e874b0b995d6ef079904ac227bc74e4a72a9465
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-10-25 09:58:38 +0200

    Bump the patch.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Jeffrey S. Haemer"
 author = "Jeffrey S. Haemer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.8"
+release = "0.1.9"
 
 
 # -- General configuration ---------------------------------------------------
