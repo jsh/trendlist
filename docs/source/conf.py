d6de74ad60035e922988200cb0f04eb4295bf99a
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2022-12-21 18:53:07 -0700

    Bump version from 0.3.6 to --help

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = "2022, Jeffrey S. Haemer"
 author = "Jeffrey S. Haemer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.6"
+release = "--help"
 
 
 # -- General configuration ---------------------------------------------------
--- src/trendlist/__init__.py
+++ src/trendlist/__init__.py
@@ -1,4 +1,4 @@
 """Import all functions from trendlist module into package."""
-__version__ = "0.3.6"
+__version__ = "--help"
 
 from .trendlist import *  # noqa
